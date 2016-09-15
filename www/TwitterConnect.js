c32516ff697771b60ebb121f116dd6bfd598fb92
mcookman <mcookman@expansionhealth.com>
2016-09-15 09:16:55 -0400

    Add files via upload

--- www/TwitterConnect.js
+++ www/TwitterConnect.js
@@ -1,15 +1,18 @@
 var exec = require('cordova/exec');
 
 var TwitterConnect = {
-	login: function (successCallback, errorCallback) {
-		exec(successCallback, errorCallback, 'TwitterConnect', 'login', []);
-	},
-	logout: function (successCallback, errorCallback) {
-		exec(successCallback, errorCallback, 'TwitterConnect', 'logout', []);
-	},
-	showUser: function (successCallback, errorCallback) {
-		exec(successCallback, errorCallback, 'TwitterConnect', 'showUser', []);
-	}
+    login: function (successCallback, errorCallback) {
+        exec(successCallback, errorCallback, 'TwitterConnect', 'login', []);
+    },
+    logout: function (successCallback, errorCallback) {
+        exec(successCallback, errorCallback, 'TwitterConnect', 'logout', []);
+    },
+    showUser: function (successCallback, errorCallback) {
+        exec(successCallback, errorCallback, 'TwitterConnect', 'showUser', []);
+    },
+    statusUpdate: function (successCallback, errorCallback, status) {
+        exec(successCallback, errorCallback, 'TwitterConnect', 'statusUpdate', [status]);
+    }
 };
 
 module.exports = TwitterConnect;
